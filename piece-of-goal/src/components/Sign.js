6ce18d83aca21672fc262f496759e495576a9178
Alice <clotilda4116@gmail.com>
2025-02-25 18:41:36 +0900

    update

--- piece-of-goal/src/components/Sign.js
+++ piece-of-goal/src/components/Sign.js
@@ -1,20 +1,15 @@
+// Sign.js
 import { auth } from "../config/firebaseConfig";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { saveUserData } from "../services/userService"; // userService에서 사용자 데이터 저장 함수 가져오기
 
+// 로그인 성공 후 navigate 추가
 export function handleGoogleLogin(setUserData, navigate) {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider)
     .then((data) => {
-      const user = data.user;
-      setUserData(user);
-      console.log(user);
-
-      // Firestore에 사용자 데이터 저장
-      saveUserData(user);
-
-      // 로그인 후 Monthly 페이지로 이동
-      navigate('/monthly');
+      setUserData(data.user);
+      console.log(data);
+      navigate('/monthly'); // 로그인 후 Monthly 페이지로 이동
     })
     .catch((err) => {
       console.log(err);
